1d5fbeef804a86f8ac0330f4835e0a4d0148335a
Todd Greenberg <tsgreenberg1217@gmail.com>
2017-08-24 05:35:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,25 @@
-# require modules here
+
+require 'yaml'
 
 def load_library
-  # code goes here
+  YAML.load_file('emoticons.yml')
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(emoticon)
+  load_library.each do |trans, emot|
+    if emot[0]==emoticon
+      return emot[1]
+    end
+  end
+
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(emoticon)
+  j=get_japanese_emoticon(emoticon)
+  load_library.each do |trans, emot|
+    if emot[1]==j
+      return trans
+    end
+  end
+
 end
