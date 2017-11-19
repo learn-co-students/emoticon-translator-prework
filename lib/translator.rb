1d65fb473487a0ff2723ad497a5e5af44c0fe268
tanelam <tania.e.aparicio@gmail.com>
2017-11-19 01:12:18 -0500

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,11 +4,15 @@ require 'pry'
 
 def load_library(file_path)
   # code goes here
-  library = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = {}
+  library["get_emoticon"] = {}
+  library["get_meaning"] = {}
+
   YAML.load_file(file_path).each do |meaning, array|
     english, japanese = array
     library["get_emoticon"][english] = japanese
     library["get_meaning"][japanese] = meaning
+
   end
   library
 end
@@ -17,8 +21,8 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   library = load_library(file_path)
 
-  if library["get_emoticon"][emoticon]
-    library["get_emoticon"][emoticon]
+  if library["get_emoticon"][emoticon] 
+     library["get_emoticon"][emoticon]
   else
     "Sorry, that emoticon was not found"
   end
@@ -27,7 +31,6 @@ end
 def get_english_meaning(file_path, emoticon)
   # code goes here
   library = load_library(file_path)
-
 
   if library["get_meaning"][emoticon]
      library["get_meaning"][emoticon]
