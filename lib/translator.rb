2d79c22642e3083963532e2ae34d504b18f268ea
Mohammad Khan <sparrowangels@gmail.com>
2018-01-20 07:10:41 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,25 +9,40 @@ def load_library(file_to_load)
   emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml_path,emoticon)
+  emoticons = load_library(yaml_path)
+  found = emoticons["get_emoticon"][emoticon]
+  !found.nil? ? found : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(yaml_path,emoticon)
+  emoticons=load_library(yaml_path)
+  # emoticon = "（＾ｖ＾）"#test
+  # puts "Did not find #{emoticon}" if emoticons["get_meaning"][emoticon].nil?
+  found = emoticons["get_meaning"][emoticon]
+  # found = emoticons["get_meaning"]
+  # puts "************************found:#{found}"
+  # puts "************************emoticon:#{emoticon}"
+  # puts "************************emoticon:#{emoticon.to_s}"
+  # puts emoticons["get_emoticon"][":)"]
+  # emoticons["get_meaning"].each do |k, v |
+  #   puts "#{k}::#{v}"
+  # end
+  !found.nil? ? found : "Sorry, that emoticon was not found"
 end
 
 
 # x = load_library
 # puts x
 x= load_library("lib/emoticons.yml")
+puts x["get_meaning"]["（＾ｖ＾）"]
 puts x["get_meaning"]["o_O"]
 puts x["get_meaning"]["\u261C(\u2312\u25BD\u2312)\u261E"]
 y= x["get_emoticon"]["O:)"]
-x.each do |k,v|
-  # puts "#{k}::#{v}"
-  v.each do |subk,subv|
-    puts "#{subk}::#{subv}"
-  end
-end
+# x.each do |k,v|
+#   # puts "#{k}::#{v}"
+#   v.each do |subk,subv|
+#     puts "#{subk}::#{subv}"
+#   end
+# end
 # puts y.is_array?
