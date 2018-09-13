0b8641825a664166056316ebe83e7ff9913797c6
Asaf Davidov <adavidov@conncoll.edu>
2018-09-13 21:37:25 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,10 +6,22 @@ def load_library(path)
   emoticons = YAML.load_file(path)
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, amer_emote)
   # code goes here
+  emoticons = load_library(path)
+  if emoticons["get_emoticon"][amer_emote] != nil
+    emoticons["get_emoticon"][amer_emote]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
+def get_english_meaning(path, japan_emote)
   # code goes here
+    emoticons = load_library(path)
+  if emoticons["get_meaning"][japan_emote] != nil
+    emoticons["get_meaning"][japan_emote]
+  else
+    return "Sorry, that emoticon was not found"
+  end
 end
