e7595b784902709bbd3db7258eaac81c51ee2c9d
github username <github email address>
2018-09-29 14:45:52 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,35 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+#    #library_hash =  {"get_meaning"=>{"☜(⌒▽⌒)☞"=>"angel"}, "get_emoticon"=>{"O:)"=>"☜(⌒▽⌒)☞"}}
+    # jap_emoticons = 'get_meaning'[emoticons[0]]" || 'get_emoticons[emoticons[1]] ==>'☜(⌒▽⌒)☞"
+    #eng emoticon = 'get_emoticons'[emoticons[0]] ==> 'O:)'
+    #meaning = 'get_meaning'[emoticons[1]]  ==>  "angel"
+
+def load_library(file_path)
+  path = YAML.load_file(file_path)
+  library_hash = { "get_meaning" => {}, "get_emoticon" => {} }
+  path.each do |meaning, emoticons|
+    library_hash["get_meaning"][emoticons[1]] = meaning   #☜(⌒▽⌒)☞" = 'angel'
+    library_hash["get_emoticon"][emoticons[0]] = emoticons[1]   #"O:)" = "☜(⌒▽⌒)☞"
+  end
+  library_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file_path, emoticon)
+  path = YAML.load_file(file_path)
+  library_hash = load_library(file_path)
+  compare_h = library_hash['get_emoticon']
+  #if eng emoticon param = w/in ({get_emoticon"=>{"O:)"=>"☜(⌒▽⌒)☞"}})
+  compare_h.include?(emoticon) ? compare_h[emoticon] : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(file_path, emoticon)
+  path = YAML.load_file(file_path)
+  library_hash = load_library(file_path)
+  define_h = library_hash['get_meaning']
+      # if jap emoticon param = w/in ({"get_meaning"=>{"☜(⌒▽⌒)☞"=>"angel"}})
+  define_h.include?(emoticon) ? define_h[emoticon] : "Sorry, that emoticon was not found"
 end
