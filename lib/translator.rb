ce4eebb4a11abcb0d279817f4f4094e9fedd1755
Malcolm Leake <echelon1089@gmail.com>
2018-11-05 22:02:35 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,17 +5,27 @@ require 'yaml'
 def load_library(chase)
   #binding.pry
   YAML.load_file(chase)
-  emotions = {'get_meaning' => {} , 'get_emoticon' => {} }
- #binding.pry
+  emotions = {'get_meaning' => {}, 'get_emoticon' => {} }
+ YAML.load_file(chase).each do |meaning , emojis|
+       emotions['get_emoticon'][emojis[0]] = emojis[1]
+       emotions['get_meaning'][emojis[1]] = meaning
+
+
+#binding.pry
 
   
 
-
+end
 emotions
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(chase, emoji)
+  load_library(chase)
+  YAML.load_file(chase).each do |meaning , emojis|
+       emotions['get_emoticon'][emojis[0]] = emojis[1]
+  
+end
+emojis[1]
 end
 
 def get_english_meaning
