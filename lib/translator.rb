0e7825d7750050fc7b3dd4db40219a0551468812
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-26 15:26:55 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,16 @@
 require 'yaml'
-
+require 'pry'
 def load_library(path)
   emoticons = YAML.load_file(path)
   hash = {"get_meaning" => {}, "get_emoticon" => {}}
-  
+  emoticons.each do |word, array_of_symbols|
+    array_of_symbols.each do |element|
+      hash["get_meaning"][array_of_symbols[1]] = word
+      hash["get_emoticon"][array_of_symbols[0]] = array_of_symbols[1]
+    end
+  end
+ # binding.pry
+hash
 end
 
 def get_japanese_emoticon(path, emoticon)
