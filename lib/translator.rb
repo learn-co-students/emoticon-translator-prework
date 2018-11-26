3c55c7630a7d8157d39457cdf6ce733c6c2300cf
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-26 14:44:30 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,15 +1,14 @@
 require 'yaml'
 
 def load_library(path)
-  emoticons = YAML.load_file(path)
   hash = {"get_meaning" => {}, "get_emoticon" => {}}
   
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  
 end
