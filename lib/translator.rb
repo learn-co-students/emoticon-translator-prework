49940d0e4185a75d531367eec16745cb2bb76216
brinda behal <brindab50@gmail.com>
2019-09-28 21:37:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,14 +3,19 @@ require "yaml"
 require 'pry'
 
 def load_library(file_path)
- new_hash = {}
+  new_hash = { 
+  :get_meaning => {},
+  :get_emoticon => {}
+  }
 emoji = YAML.load_file(file_path)
 emoji.each do |key, value|
+  
+  new_hash[:get_meaning][value[1]] = key
+  new_hash[:get_emoticon][value[0]] = value[1]
+  #binding.pry
 end
-new_hash[:get_meaning] = emoji 
-new_hash[:get_emoticon] = emoji
 new_hash
- #binding.pry
+
 end
 
 def get_japanese_emoticon
