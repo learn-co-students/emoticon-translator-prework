e6a2e2d92b53d41184e843d3beac8ad9f5c72581
brinda behal <brindab50@gmail.com>
2019-09-30 00:13:01 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,6 +26,11 @@ def get_japanese_emoticon(file_path, emoticon)
       
   end 
 end 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
    new_hash = load_library(file_path)
-end
+   if new_hash[:get_meaning][emoticon]
+     return new_hash[:get_meaning][emoticon]
+   else
+     "Sorry, that emoticon was not found"
+  end
+end 
